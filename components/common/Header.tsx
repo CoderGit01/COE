0848ef8a8037bedb963cc69717f3228807f082d7
Lobsang Tsetan Shakya <114970676+lobsangshakya@users.noreply.github.com>
2025-09-30 09:37:12 +0530

    Update Header.tsx

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -89,7 +89,7 @@ const Header = () => {
                     <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <NavLink
                         to={getDashboardPath()}
-                        className="block px-4 py-2 text-sm text-black-700 hover:bg-gray-100"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                         onClick={() => setDropdownOpen(false)}
                       >
                         Dashboard
